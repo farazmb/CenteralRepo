0c4dbb154b6d47f0c67a8ed00e3d71a6077a089b
Faraz <124167889+farazmb@users.noreply.github.com>
2025-09-08 11:24:36 +0500

    Initial commit

--- dist/PracRXJS.js
+++ dist/PracRXJS.js
@@ -1,4 +1,32 @@
-import { Observable } from 'rxjs';
-const obs$ = new Observable(sub => sub.next('Hello World!'));
-obs$.subscribe(val => console.log(val));
+import { firstValueFrom, interval, Observable, of } from 'rxjs';
+//const numbers$ = interval(1000 );
+//  numbers$.subscribe(num => console.log(`I ${num} dog shit`));
+// const numbers$ = of(10, 20, 30);
+// numbers$.subscribe({
+//   next: val => console.log('Value:', val),
+//   complete: () => console.log('Stream ended')
+// });
+// const nums$ = of(100, 200, 300, 400, 500, 600, 700, 800, 900);
+// nums$.subscribe({
+//     next: val => console.log('Number:', val),
+//     complete: () => console.log('Number stream ended')
+// })
+//stream example
+// const numbers$ = interval(1000);
+// const subscription = numbers$.subscribe({
+//     next: val => console.log('Interval Value:', val),
+//     complete: () => console.log('Interval stream ended')
+// })
+//overservables
+const hello$ = new Observable(subscriber => {
+    subscriber.next('Hello');
+    subscriber.next('World');
+    subscriber.complete();
+});
+hello$.subscribe({
+    next: val => console.log('Message:', val),
+    complete: () => console.log('Message stream ended')
+});
+const res = await firstValueFrom(hello$);
+console.log('First Value from hello$:', res);
 //# sourceMappingURL=PracRXJS.js.map
